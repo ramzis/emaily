f564978fa60033dfe4ef85335de5ee2b9d5d279e
Tadas Audinis <ta15668@my.bristol.ac.uk>
2019-05-08 11:56:22 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import express from "express";
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send({ hey: "there" });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
