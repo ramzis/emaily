ae0f8d04663467d032a68c1fea49388a7fdd3345
Tadas Audinis <ta15668@my.bristol.ac.uk>
2019-05-10 18:49:15 -0700

    Add webpack

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const nodeExternals = require("webpack-node-externals");
+
+module.exports = {
+  entry: {
+    server: "./src/index.js"
+  },
+  output: {
+    path: path.join(__dirname, "dist"),
+    publicPath: "/",
+    filename: "index.js"
+  },
+  target: "node",
+  node: {
+    __dirname: false,
+    __filename: false
+  },
+  externals: [nodeExternals()],
+  module: {
+    rules: [
+      {
+        // Transpiles ES6-8 into ES5
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  }
+};
